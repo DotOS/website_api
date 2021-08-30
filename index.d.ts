fbe559966ab00e33154b3fd245f245948546a613
ririxidev <mail@ririxi.dev>
2021-08-30 19:46:02 +0200

    (releases): Add images

--- index.d.ts
+++ index.d.ts
@@ -1,13 +1,13 @@
 interface Device {
 	codename: string | string[];
 	deviceName: string;
-	brand: string;
-	ota: boolean;
-	latest: string;
+	brandName: string;
+	otaEnabled: boolean;
+	latestVersion: string;
 	discontinued: boolean;
 	deviceChangelog: Changelog[] | null;
 	releases: Releases[] | null;
-	maintainer: Maintainer;
+	maintainerInfo: Maintainer;
 	links: Links;
 }
 interface Changelog {
@@ -21,9 +21,14 @@ interface Releases {
 	fileName: string;
 	url: string;
 	requireCleanFlash: boolean;
+	images: Images[] | null;
 	size: number;
 	version: string;
 	latest: boolean;
+}
+interface Images {
+	type: "boot" | "recovery";
+	url: string;
 }
 interface Maintainer {
 	name: string | null;
